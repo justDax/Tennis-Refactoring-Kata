05e884a6a6c1480ec0bfda4841d6791aa901ba58
Daniele Ciffo <11134414+justDax@users.noreply.github.com>
2021-04-10 17:36:02 +0200

    renames variables to be more meaningful

--- ruby/tennis_game3.rb
+++ ruby/tennis_game3.rb
@@ -1,30 +1,30 @@
 class TennisGame3
   def initialize(player1Name, player2Name)
-    @p1N = player1Name
-    @p2N = player2Name
-    @p1 = 0
-    @p2 = 0
+    @player1_name = player1Name
+    @player2_name = player2Name
+    @player1_score = 0
+    @player2_score = 0
   end
       
   def won_point(n)
-    if n == @p1N
-        @p1 += 1
+    if n == @player1_name
+        @player1_score += 1
     else
-        @p2 += 1
+        @player2_score += 1
     end
   end
   
   def score
-    if (@p1 < 4 and @p2 < 4) and (@p1 + @p2 < 6)
+    if (@player1_score < 4 and @player2_score < 4) and (@player1_score + @player2_score < 6)
       p = ["Love", "Fifteen", "Thirty", "Forty"]
-      s = p[@p1]
-      @p1 == @p2 ? s + "-All" : s + "-" + p[@p2]
+      s = p[@player1_score]
+      @player1_score == @player2_score ? s + "-All" : s + "-" + p[@player2_score]
     else
-      if (@p1 == @p2)
+      if (@player1_score == @player2_score)
         "Deuce"
       else
-        s = @p1 > @p2 ? @p1N : @p2N
-        (@p1-@p2)*(@p1-@p2) == 1 ? "Advantage " + s : "Win for " + s
+        s = @player1_score > @player2_score ? @player1_name : @player2_name
+        (@player1_score-@player2_score)*(@player1_score-@player2_score) == 1 ? "Advantage " + s : "Win for " + s
       end
     end
   end
