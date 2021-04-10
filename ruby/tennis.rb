ad5050dbce463080830a87caadea6d043d6ba2f8
Daniele Ciffo <11134414+justDax@users.noreply.github.com>
2021-04-10 16:47:36 +0200

    begins refactoring for TennisGame2, formats spacing and camel_cases variable names

--- ruby/tennis.rb
+++ ruby/tennis.rb
@@ -50,133 +50,135 @@ class TennisGame1
   
 end
 
+
+
 class TennisGame2
-  def initialize(player1Name, player2Name)
-    @player1Name = player1Name
-    @player2Name = player2Name
-    @p1points = 0
-    @p2points = 0
+  def initialize(player1_name, player2_name)
+    @player1_name = player1_name
+    @player2_name = player2_name
+    @p1_points = 0
+    @p2_points = 0
   end
       
-  def won_point(playerName)
-    if playerName == @player1Name
-      p1Score()
+  def won_point(player_name)
+    if player_name == @player1_name
+      p1_score()
     else
-      p2Score()
+      p2_score()
     end
   end
 
   def score
     result = ""
-    if (@p1points == @p2points and @p1points < 3)
-      if (@p1points==0)
+    if (@p1_points == @p2_points and @p1_points < 3)
+      if (@p1_points == 0)
         result = "Love"
       end
-      if (@p1points==1)
+      if (@p1_points == 1)
         result = "Fifteen"
       end
-      if (@p1points==2)
+      if (@p1_points == 2)
         result = "Thirty"
       end
       result += "-All"
     end
-    if (@p1points==@p2points and @p1points>2)
-        result = "Deuce"
+    if (@p1_points == @p2_points and @p1_points > 2)
+      result = "Deuce"
     end
     
-    p1res = ""
-    p2res = ""
-    if (@p1points > 0 and @p2points==0)
-      if (@p1points==1)
-        p1res = "Fifteen"
+    p1_res = ""
+    p2_res = ""
+    if (@p1_points > 0 and @p2_points == 0)
+      if (@p1_points == 1)
+        p1_res = "Fifteen"
       end
-      if (@p1points==2)
-        p1res = "Thirty"
+      if (@p1_points == 2)
+        p1_res = "Thirty"
       end
-      if (@p1points==3)
-        p1res = "Forty"
+      if (@p1_points == 3)
+        p1_res = "Forty"
       end
-      p2res = "Love"
-      result = p1res + "-" + p2res
+      p2_res = "Love"
+      result = p1_res + "-" + p2_res
     end
-    if (@p2points > 0 and @p1points==0)
-      if (@p2points==1)
-        p2res = "Fifteen"
+    if (@p2_points > 0 and @p1_points == 0)
+      if (@p2_points == 1)
+        p2_res = "Fifteen"
       end
-      if (@p2points==2)
-        p2res = "Thirty"
+      if (@p2_points == 2)
+        p2_res = "Thirty"
       end
-      if (@p2points==3)
-        p2res = "Forty"
+      if (@p2_points == 3)
+        p2_res = "Forty"
       end
       
-      p1res = "Love"
-      result = p1res + "-" + p2res
+      p1_res = "Love"
+      result = p1_res + "-" + p2_res
     end
     
-    if (@p1points>@p2points and @p1points < 4)
-      if (@p1points==2)
-        p1res="Thirty"
+    if (@p1_points > @p2_points and @p1_points < 4)
+      if (@p1_points == 2)
+        p1_res = "Thirty"
       end
-      if (@p1points==3)
-        p1res="Forty"
+      if (@p1_points == 3)
+        p1_res = "Forty"
       end
-      if (@p2points==1)
-        p2res="Fifteen"
+      if (@p2_points == 1)
+        p2_res="Fifteen"
       end
-      if (@p2points==2)
-        p2res="Thirty"
+      if (@p2_points == 2)
+        p2_res = "Thirty"
       end
-      result = p1res + "-" + p2res
+      result = p1_res + "-" + p2_res
     end
-    if (@p2points>@p1points and @p2points < 4)
-      if (@p2points==2)
-        p2res="Thirty"
+    if (@p2_points > @p1_points and @p2_points < 4)
+      if (@p2_points == 2)
+        p2_res = "Thirty"
       end
-      if (@p2points==3)
-        p2res="Forty"
+      if (@p2_points == 3)
+        p2_res="Forty"
       end
-      if (@p1points==1)
-        p1res="Fifteen"
+      if (@p1_points == 1)
+        p1_res = "Fifteen"
       end
-      if (@p1points==2)
-        p1res="Thirty"
+      if (@p1_points == 2)
+        p1_res = "Thirty"
       end
-      result = p1res + "-" + p2res
+      result = p1_res + "-" + p2_res
     end
-    if (@p1points > @p2points and @p2points >= 3)
-      result = "Advantage " + @player1Name
+    if (@p1_points > @p2_points and @p2_points >= 3)
+      result = "Advantage " + @player1_name
     end
-    if (@p2points > @p1points and @p1points >= 3)
-      result = "Advantage " + @player2Name
+    if (@p2_points > @p1_points and @p1_points >= 3)
+      result = "Advantage " + @player2_name
     end
-    if (@p1points>=4 and @p2points>=0 and (@p1points-@p2points)>=2)
-      result = "Win for " + @player1Name
+    if (@p1_points >= 4 and @p2_points >= 0 and (@p1_points - @p2_points) >= 2)
+      result = "Win for " + @player1_name
     end
-    if (@p2points>=4 and @p1points>=0 and (@p2points-@p1points)>=2)
-      result = "Win for " + @player2Name
+    if (@p2_points >= 4 and @p1_points >= 0 and (@p2_points - @p1_points) >= 2)
+      result = "Win for " + @player2_name
     end
     result
   end
 
-  def setp1Score(number)
+  def setp1_score(number)
     (0..number).each do |i|
         p1Score()
     end
   end
 
-  def setp2Score(number)
+  def setp2_score(number)
     (0..number).each do |i|
       p2Score()
     end
   end
 
-  def p1Score
-    @p1points +=1
+  def p1_score
+    @p1_points += 1
   end
   
-  def p2Score
-    @p2points +=1
+  def p2_score
+    @p2_points += 1
   end
 end
 
