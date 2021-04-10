2d65c5397ca803fafcc42321c18e41f40bb8d666
Daniele Ciffo <11134414+justDax@users.noreply.github.com>
2021-04-10 18:27:43 +0200

    refactor to store the player data in an hash

--- ruby/tennis_game3.rb
+++ ruby/tennis_game3.rb
@@ -3,29 +3,42 @@ class TennisGame3
 
 
   def initialize(player1Name, player2Name)
-    @player1_name = player1Name
-    @player2_name = player2Name
-    @player1_score = @player2_score = 0
+    @players = {}               # This hash represents the players with a playerName as the key and his/her score as value
+                                # while with this logic won't work when both players have the same name, 
+                                # I assume that this case will never happen otherwise it won't be possible 
+                                # to add points by player name too
+    @players[player1Name] = 0
+    @players[player2Name] = 0
   end
      
   
   def won_point(player_name)
-    if player_name == @player1_name
-      @player1_score += 1
-    else
-      @player2_score += 1
-    end
+    @players[player_name] += 1
   end
   
+
+  def scores
+    @players.values
+  end
+
   
+  # the #player1_score and the #player2_score methods have been added for readability
+  def player1_score
+    scores.first
+  end
+  
+  def player2_score
+    scores[1]
+  end
+
+  
+
   def score
-    if (@player1_score < 4 and @player2_score < 4) and (@player1_score + @player2_score < 6)
-      SCORE_WORDS[@player1_score] + "-" + (@player1_score == @player2_score ? "All" : SCORE_WORDS[@player2_score])
+    if (player1_score < 4 and player2_score < 4) && scores.sum < 6
+      SCORE_WORDS[player1_score] + "-" + (player1_score == player2_score ? "All" : SCORE_WORDS[player2_score])
     else
-      return "Deuce" if @player1_score == @player2_score
-      
-      winning_player_name = @player1_score > @player2_score ? @player1_name : @player2_name
-      ((@player1_score - @player2_score).abs == 1 ? "Advantage " : "Win for ") + winning_player_name
+      return "Deuce" if player1_score == player2_score
+      (scores.inject(&:-).abs == 1 ? "Advantage " : "Win for ") + @players.key(scores.max)
     end
   end
 
