542506659ffefaf513009b792096ed8e1fc40eae
Daniele Ciffo <11134414+justDax@users.noreply.github.com>
2021-04-10 17:53:21 +0200

    turns score_words into a constant, shortens #score method

--- ruby/tennis_game3.rb
+++ ruby/tennis_game3.rb
@@ -1,11 +1,15 @@
 class TennisGame3
+  SCORE_WORDS = ["Love", "Fifteen", "Thirty", "Forty"].freeze
+
+  
   def initialize(player1Name, player2Name)
     @player1_name = player1Name
     @player2_name = player2Name
     @player1_score = 0
     @player2_score = 0
   end
-      
+     
+  
   def won_point(n)
     if n == @player1_name
       @player1_score += 1
@@ -14,18 +18,17 @@ class TennisGame3
     end
   end
   
+  
   def score
     if (@player1_score < 4 and @player2_score < 4) and (@player1_score + @player2_score < 6)
-      score_words = ["Love", "Fifteen", "Thirty", "Forty"]
-      score_word = score_words[@player1_score]
-      @player1_score == @player2_score ? score_word + "-All" : score_word + "-" + score_words[@player2_score]
+      player1_score_word = SCORE_WORDS[@player1_score]
+      @player1_score == @player2_score ? player1_score_word + "-All" : player1_score_word + "-" + SCORE_WORDS[@player2_score]
     else
-      if (@player1_score == @player2_score)
-        "Deuce"
-      else
-        winning_player_name = @player1_score > @player2_score ? @player1_name : @player2_name
-        (@player1_score - @player2_score) * (@player1_score - @player2_score) == 1 ? "Advantage " + winning_player_name : "Win for " + winning_player_name
-      end
+      return "Deuce" if @player1_score == @player2_score
+      
+      winning_player_name = @player1_score > @player2_score ? @player1_name : @player2_name
+      (@player1_score - @player2_score).abs == 1 ? "Advantage " + winning_player_name : "Win for " + winning_player_name
     end
   end
+
 end
